03146765ca9f7cd6a5e2992f43ad3c0e3f0424f1
Sonia Briet <qualioexpert@gmail.com>
2026-07-28 12:19:17 +0200

    Ajout des balises Open Graph avec la boussole comme image de partage

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // Configuration Astro
-// TODO : remplacer par votre vrai nom de domaine une fois réservé
 export default defineConfig({
-  site: 'https://www.votre-domaine.fr',
+  site: 'https://qualioexpert.fr',
 });
